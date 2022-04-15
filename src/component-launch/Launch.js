1b893e9f75dca2b8e4ae580e4a6b230ac436661c
Dinergo <58221330+Dinergo@users.noreply.github.com>
2022-04-15 12:46:32 +0200

    écriture de Launch permettant de récupérer les données d'un vol qui lui est passé en paramètre

--- src/component-launch/Launch.js
+++ src/component-launch/Launch.js
@@ -5,8 +5,13 @@ import React, { Component } from 'react';
 class launch extends React.Component {
     constructor(props) {
         super(props);
+        
+
+
+        // version 1 du state
+        /*
         this.state = {
-            numero_vol: props.flight_numeber,
+            numero_vol: props.flight_number,
             mission: props.mission_name,
             annee: props.launch_year,
             dateExacte: props.launch_date_local,
@@ -14,12 +19,27 @@ class launch extends React.Component {
             site_lancement: props.launch_site,
             launch_success: props.launch_success,
             links: props.links
-            };
+            };*/
+           
+        this.state = {
+            data : props.data  
+        }
+
     }
+
+    componentDidMount() {
+        this.setState((state, props) => ({
+            date: props.data
+        }))
+    }
+    
     render() {
         return (
             <div>
-                launch
+                <div>
+                    numero de vol : {this.state.data.flight_number}
+                </div>
+
             </div>
         )
     }

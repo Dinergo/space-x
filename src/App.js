60f789c8abcca269691f5f25b4ce544fcbd5cafe
Dinergo <58221330+Dinergo@users.noreply.github.com>
2022-04-15 11:28:00 +0200

    nettoyage de code & commentaires inutile

--- src/App.js
+++ src/App.js
@@ -18,26 +18,6 @@ class App extends React.Component {
    componentDidMount() {
     
 
-    // version 2
-    /*
-    const axios = require('axios').default
-
-    try {
-      const response = await axios.get("https://api.spacexdata.com/v3/launches/64");
-      console.log(response);
-      this.setState((state) => ({
-        lancement : response.data
-      }));
-      console.log(this.state.lancement);
-    } catch (error) {
-      console.error(error);
-    };
-    
-    console.log(this.state.lancement);
-    */
-
-    // version 1
-    
     axios.get('https://api.spacexdata.com/v3/launches/64')
     .then( (response) => {
       console.log(response);
@@ -46,9 +26,8 @@ class App extends React.Component {
       console.log(this.lancement);
       console.log("fin de la requete");
     });
-    console.log("post-request");
-    console.log(this.state.lancement);
-  // */
+    
+  
 
   }
   
